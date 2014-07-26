b42db989a0a4f42dc74724527fdf24d460cd1f1b
Paul Wenig <pwenig@gmail.com>
2014-07-26 16:45:44 -0600

    Added unique categories validations

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,4 +1,4 @@
 class Categorization < ActiveRecord::Base
 belongs_to :category
-  validates_presence_of :category, :message => "Category can't be blank"
+  validates_presence_of :category, :message => "Category can't be blank", uniqueness: {scope: :kitten_id}
 end
--- spec/models/categorization_spec.rb
+++ spec/models/categorization_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 
 describe Categorization do
 
-it "category cannot be blank" do
-  categorization = Categorization.new(category: Category.new)
-  expect(categorization).to have(0).errors_on(:category)
+  it "category cannot be blank" do
+    categorization = Categorization.new(category: Category.new)
+    expect(categorization).to have(0).errors_on(:category)
 
-  categorization.category = nil
-  expect(categorization).to have(1).errors_on(:category)
-end
+    categorization.category = nil
+    expect(categorization).to have(1).errors_on(:category)
+  end
+
+  it "validates uniqueness of categor for each kitten" do
+    category = Category.create(name: "Cute category")
+    kitten = Kitten.create(image: "any url")
+
+    Categorization.create(kitten_id: kitten.id, category: category)
+
+    categorization = Categorization.new(category: category)
+    expect(categorization).to have(0).errors_on(:category)
+  end
 
 end
